9a7cf9ffb5ea9a6c5a3989b69f343df91cc37eb9
starmlerp <filip.jamuljak@gmail.com>
2022-01-29 11:58:29 +0100

    added README and modeified the way the code works a bit

---
+++ README.md
@@ -0,0 +1,12 @@
+##i3 session manager
+this is a small script, intended to be integrated into i3 as a keybind/mode/whatever
+##usage
+This script was namely intended to be used as a form of session recovery tool
+in case of a system crash.
+First, edit the script variables to suit your needs. See section variables.
+Invoke the command with:
+```sh
+$ i3-mngr.sh load|save [NAME]
+```
+Where NAME is the optional argument asking for the calling name of the workspace we want to save.
+
--- i3-mngr.sh
+++ i3-mngr.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-attributes="class instance"
+attributes="class instance" #json attributes we wish to automatically uncomment
 sessiondir=~/.i3-sessions
+format=$sessiondir/session$2.json
+
+#this is bodgy, and wont work should the guys at i3 decide to change the output
+#format of the i3-msg command but it works in this very specific case scenario,
+#and i see no much other way to implement it that doesnt involve me developing
+#my own text truncating solution
 workspaces=$(i3-msg -t get_workspaces | sed -e "s/,/\n/g" | grep name | sed -e "s/\"name\":\"//" | sed -e "s/\"//")
-session=$sessiondir/session$2.json
 
 if [[ $1 == "save" ]]; then
-	if [[ $2 == "all" ]]; then
+	if [[ $2 == "" ]]; then
 		for i in $workspaces; do
 			i3-save-tree --workspace=$i > $sessiondir/session$i.json
 			for j in $attributes; do
@@ -14,18 +19,18 @@ if [[ $1 == "save" ]]; then
 			done
 		done
 	else
-		i3-save-tree --workspace=$2 > $session
+		i3-save-tree --workspace=$2 > $format
 		for i in $attributes; do
-			sed -i "s/\/\/ \"$i\"/\"$i\"/" $session
+			sed -i "s/\/\/ \"$i\"/\"$i\"/" $format
 		done
 	fi
 elif [[ $1 == "load" ]]; then
-	if [[ $2 == "all" ]]; then
+	if [[ $2 == "" ]]; then
 		for i in $(ls $sessiondir | sed -e "s/session//" -e "s/.json//"); do
 			i3-msg "workspace $i; append_layout $sessiondir/session$1.json"
 		done
 	else
-		i3-msg "workspace $1; append_layout $session"
+		i3-msg "workspace $1; append_layout $format"
 	fi	
 else echo "no matches found"
 fi
